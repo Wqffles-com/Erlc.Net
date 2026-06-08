d35a5170a9c6f48ce9c894da5490a78816cff399
Wqffles-com <wqffles@astrolink.cc>
2026-06-08 17:55:33 +0200

    feat: added caching and client injection and stuff :100:

--- Core/ErlcClient.cs
+++ Core/ErlcClient.cs
@@ -7,8 +7,28 @@ namespace Core;
 
 public class ErlcClient : IDisposable
 {
+    private static readonly TimeSpan ServerInfoPollingInterval = TimeSpan.FromSeconds(15);
+
     private readonly HttpClient _httpClient;
     private readonly JsonSerializerSettings _serializerSettings;
+    private readonly CancellationTokenSource _serverInfoEventsCancellationTokenSource = new();
+    private readonly Task _serverInfoEventsTask;
+    private ServerInfo? _cachedServerInfo;
+    private bool _disposed;
+
+    public event EventHandler<ServerInfoChangedEventArgs>? ServerDetailsChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<List<Player>>>? PlayersChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<Staff>>? StaffChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<Dictionary<string, string>>>? AdminsChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<Dictionary<string, string>>>? ModsChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<Dictionary<string, string>>>? HelpersChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<List<JoinLog>>>? JoinLogsChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<List<int>>>? QueueChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<List<KillLog>>>? KillLogsChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<List<CommandLog>>>? CommandLogsChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<List<ModCall>>>? ModCallsChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<List<EmergencyCall>>>? EmergencyCallsChanged;
+    public event EventHandler<ServerInfoChangedEventArgs<List<Vehicle>>>? VehiclesChanged;
 
     public ErlcClient(string apiKey, HttpClient? httpClient = null)
     {
@@ -24,6 +44,8 @@ public class ErlcClient : IDisposable
         {
             Context = new StreamingContext(StreamingContextStates.Other, this)
         };
+
+        _serverInfoEventsTask = RunServerInfoEventsAsync(_serverInfoEventsCancellationTokenSource.Token);
     }
 
     public async Task<ServerInfo> GetServerInfoAsync(
@@ -55,7 +77,12 @@ public class ErlcClient : IDisposable
         
         var json = await response.Content.ReadAsStringAsync();
         var serverInfo = JsonConvert.DeserializeObject<ServerInfo>(json, _serializerSettings);
-        
+
+        if (serverInfo != null)
+        {
+            InjectClient(serverInfo);
+        }
+
         return serverInfo ?? throw new InvalidOperationException("Failed to deserialize server info.");
     }
 
@@ -71,6 +98,180 @@ public class ErlcClient : IDisposable
 
     public void Dispose()
     {
+        if (_disposed)
+        {
+            return;
+        }
+
+        _disposed = true;
+        _serverInfoEventsCancellationTokenSource.Cancel();
+        _serverInfoEventsCancellationTokenSource.Dispose();
         _httpClient.Dispose();
     }
+
+    private async Task RunServerInfoEventsAsync(CancellationToken cancellationToken)
+    {
+        using var timer = new PeriodicTimer(ServerInfoPollingInterval);
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                await PollServerInfoAsync();
+                await timer.WaitForNextTickAsync(cancellationToken);
+            }
+            catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
+            {
+                break;
+            }
+            catch
+            {
+                await timer.WaitForNextTickAsync(cancellationToken);
+            }
+        }
+    }
+
+    private async Task PollServerInfoAsync()
+    {
+        var serverInfo = await GetServerInfoAsync(
+            players: true,
+            staff: true,
+            joinLogs: true,
+            queue: true,
+            killLogs: true,
+            commandLogs: true,
+            modCalls: true,
+            emergencyCalls: true,
+            vehicles: true);
+
+        var cachedServerInfo = _cachedServerInfo;
+        if (cachedServerInfo != null)
+        {
+            RaiseChangedEvents(cachedServerInfo, serverInfo);
+        }
+
+        _cachedServerInfo = serverInfo;
+    }
+
+    private void RaiseChangedEvents(ServerInfo previous, ServerInfo current)
+    {
+        if (IsServerDetailsChanged(previous, current))
+        {
+            ServerDetailsChanged?.Invoke(this, new ServerInfoChangedEventArgs(previous, current));
+        }
+
+        RaiseIfChanged(PlayersChanged, previous.Players, current.Players, previous, current);
+        RaiseIfChanged(StaffChanged, previous.Staff, current.Staff, previous, current);
+        RaiseIfChanged(AdminsChanged, previous.Staff.Admins, current.Staff.Admins, previous, current);
+        RaiseIfChanged(ModsChanged, previous.Staff.Mods, current.Staff.Mods, previous, current);
+        RaiseIfChanged(HelpersChanged, previous.Staff.Helpers, current.Staff.Helpers, previous, current);
+        RaiseIfChanged(JoinLogsChanged, previous.JoinLogs, current.JoinLogs, previous, current);
+        RaiseIfChanged(QueueChanged, previous.Queue, current.Queue, previous, current);
+        RaiseIfChanged(KillLogsChanged, previous.KillLogs, current.KillLogs, previous, current);
+        RaiseIfChanged(CommandLogsChanged, previous.CommandLogs, current.CommandLogs, previous, current);
+        RaiseIfChanged(ModCallsChanged, previous.ModCalls, current.ModCalls, previous, current);
+        RaiseIfChanged(EmergencyCallsChanged, previous.EmergencyCalls, current.EmergencyCalls, previous, current);
+        RaiseIfChanged(VehiclesChanged, previous.Vehicles, current.Vehicles, previous, current);
+    }
+
+    private static bool IsServerDetailsChanged(ServerInfo previous, ServerInfo current)
+    {
+        return previous.Name != current.Name ||
+               previous.OwnerId != current.OwnerId ||
+               !AreEqual(previous.CoOwnerIds, current.CoOwnerIds) ||
+               previous.CurrentPlayers != current.CurrentPlayers ||
+               previous.MaxPlayers != current.MaxPlayers ||
+               previous.JoinKey != current.JoinKey ||
+               previous.AccVerifiedReq != current.AccVerifiedReq ||
+               previous.TeamBalance != current.TeamBalance;
+    }
+
+    private void RaiseIfChanged<T>(
+        EventHandler<ServerInfoChangedEventArgs<T>>? handler,
+        T previous,
+        T current,
+        ServerInfo previousServerInfo,
+        ServerInfo currentServerInfo)
+    {
+        if (!AreEqual(previous, current))
+        {
+            handler?.Invoke(this, new ServerInfoChangedEventArgs<T>(previous, current, previousServerInfo, currentServerInfo));
+        }
+    }
+
+    private static bool AreEqual<T>(T previous, T current)
+    {
+        return JsonConvert.SerializeObject(previous) == JsonConvert.SerializeObject(current);
+    }
+
+    private void InjectClient(object value)
+    {
+        if (value is BaseEntity entity)
+        {
+            entity.Client = this;
+        }
+
+        if (value is string)
+        {
+            return;
+        }
+
+        if (value is System.Collections.IEnumerable enumerable)
+        {
+            foreach (var item in enumerable)
+            {
+                if (item != null)
+                {
+                    InjectClient(item);
+                }
+            }
+
+            return;
+        }
+
+        var type = value.GetType();
+        if (type.IsPrimitive || type.IsEnum || type == typeof(decimal) || type == typeof(DateTime))
+        {
+            return;
+        }
+
+        foreach (var property in type.GetProperties())
+        {
+            if (!property.CanRead || property.GetIndexParameters().Length > 0)
+            {
+                continue;
+            }
+
+            var propertyValue = property.GetValue(value);
+            if (propertyValue != null)
+            {
+                InjectClient(propertyValue);
+            }
+        }
+    }
+}
+
+public class ServerInfoChangedEventArgs : EventArgs
+{
+    public ServerInfoChangedEventArgs(ServerInfo previousServerInfo, ServerInfo currentServerInfo)
+    {
+        PreviousServerInfo = previousServerInfo;
+        CurrentServerInfo = currentServerInfo;
+    }
+
+    public ServerInfo PreviousServerInfo { get; }
+    public ServerInfo CurrentServerInfo { get; }
+}
+
+public class ServerInfoChangedEventArgs<T> : ServerInfoChangedEventArgs
+{
+    public ServerInfoChangedEventArgs(T previous, T current, ServerInfo previousServerInfo, ServerInfo currentServerInfo)
+        : base(previousServerInfo, currentServerInfo)
+    {
+        Previous = previous;
+        Current = current;
+    }
+
+    public T Previous { get; }
+    public T Current { get; }
 }
